1225f7e1bbfd8d640c7293087e4313d8018d2576
StevenHernandez <steven.hernandez@peopleshores.com>
2024-10-13 18:46:01 -0400

    Project is setup to work with SpringBoot and Angular

---
+++ modules/ng.bat
@@ -0,0 +1,4 @@
+@echo off
+"%USERPROFILE%\AppData\Roaming\npm\ng" %*
+@REM This is the location of node installed in the work computers in order to be able to run node and angular
+@REM C:\Users\StevenHernandez\AppData\Roaming\npm\ng %*
