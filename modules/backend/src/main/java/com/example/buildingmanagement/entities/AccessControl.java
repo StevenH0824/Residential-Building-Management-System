2e4420f556ef683cb97c9fd0e559a5f100f87d3d
Dana <dana.seidakhmetova@peopleshores.com>
2024-10-14 12:05:28 -0400

    testing to see if AccessControl is updated

--- modules/backend/src/main/java/com/example/buildingmanagement/entities/AccessControl.java
+++ modules/backend/src/main/java/com/example/buildingmanagement/entities/AccessControl.java
@@ -1,2 +1,24 @@
-package com.example.buildingmanagement.entities;public class AccessControl {
+package com.example.buildingmanagement.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.util.HashMap;
+// git check comment
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Getter
+@Setter
+public class AccessControl {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private Long cardScannerId;
+  private HashMap<Long, Boolean> floorAccess;
+
 }
