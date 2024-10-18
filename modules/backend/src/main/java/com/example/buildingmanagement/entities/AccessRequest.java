749e7944f1b26d66edf9b55df3c8aacbcdb8b110
Steven <134742313+StevenH0824@users.noreply.github.com>
2024-10-17 21:38:32 -0400

    fixed a change in the previous commit regarding processedBy field

--- modules/backend/src/main/java/com/example/buildingmanagement/entities/AccessRequest.java
+++ modules/backend/src/main/java/com/example/buildingmanagement/entities/AccessRequest.java
@@ -29,7 +29,24 @@ public class AccessRequest {
   private LocalDateTime requestedDate;
   private LocalDateTime decisionDate;
   private String reason;
-  private String processedBy;  // Admin or system processing the request
+
+  @ManyToOne
+  @JoinColumn(name = "processed_by")
+  private Person processedBy;
+  /*
+  commit b4f5f100db923d3a95bf887b69065c674db88031 (origin/danaBranch)
+  Author: Dana <dana.seidakhmetova@peopleshores.com>
+  Date:   Thu Oct 17 15:44:36 2024 -0400
+  Changed the accessRequest filed from Person accessedBy to String
+  This change causes an error in the database, Spring boot still runs but this creates
+  ambiguity between processedBy which is the primary key and foreign key of another table.
+  Check the ER diagram in dbeaver for more information.
+
+  I added the Person processedBy id back as the latest git changes. We can try to see how to
+  incorportate processedBy as a String id but for now, no errors are given at all.
+   */
+
+  //private String processedBy;  // Admin or system processing the request
   @ManyToOne
   @JoinColumn(name = "floor_id")
   private Floor floor;
