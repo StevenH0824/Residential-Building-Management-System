87045f875631625fb8e87585a08600708feacff2
adesuwaE <adesuwa411@gmail.com>
2024-10-25 16:03:08 -0400

    "Build Service Class test"

---
+++ modules/backend/src/Test/java/com/example/buildingmanagement/service/BuildingServiceTest.java
@@ -0,0 +1,24 @@
+package com.example.buildingmanagement.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BuildingServiceTest {
+
+  @Test
+  void getAllBuildings() {
+  }
+
+  @Test
+  void getBuildingById() {
+  }
+
+  @Test
+  void saveBuilding() {
+  }
+
+  @Test
+  void deleteBuilding() {
+  }
+}
