fb3b97901a97aaafb1e6e377f5b88d65ff28fb0b
adesuwaE <adesuwa411@gmail.com>
2024-10-28 16:31:27 -0400

    "updated AccessLogDto"

--- modules/backend/src/main/java/com/example/buildingmanagement/dtos/AccessLogDto.java
+++ modules/backend/src/main/java/com/example/buildingmanagement/dtos/AccessLogDto.java
@@ -3,9 +3,14 @@ package com.example.buildingmanagement.dtos;
 import com.example.buildingmanagement.entities.CardScanner;
 import com.example.buildingmanagement.entities.Person;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AccessLogDto {
 
   private Long accessLogId;
