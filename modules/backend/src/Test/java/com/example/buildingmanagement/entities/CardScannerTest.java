f6c0a9f4f82e2c287df392e853fc725521feae30
StevenHernandez <steven.hernandez@peopleshores.com>
2024-10-28 15:15:54 -0400

    kim's changes to the CardScanner and Person Test files

--- modules/backend/src/Test/java/com/example/buildingmanagement/entities/CardScannerTest.java
+++ modules/backend/src/Test/java/com/example/buildingmanagement/entities/CardScannerTest.java
@@ -2,64 +2,66 @@ package com.example.buildingmanagement.entities;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
+@SpringBootTest
 class CardScannerTest {
+  CardScanner cardScanner;
 
   @BeforeEach
   void setUp() {
+    cardScanner = new CardScanner();
   }
 
   @Test
   void getCardScannerId() {
+    cardScanner.setCardScannerId(1L);
+    assertEquals(1L, cardScanner.getCardScannerId());
   }
 
   @Test
   void getSerialNo() {
+    String serialNumber = "123";
+    cardScanner.setSerialNo(serialNumber);
+    assertEquals(serialNumber, cardScanner.getSerialNo());
   }
 
   @Test
   void getMake() {
+    String make = "bestMake";
+    cardScanner.setMake(make);
+    assertEquals(make, cardScanner.getMake());
   }
 
   @Test
   void getModel() {
+    String model = "bestModel";
+    cardScanner.setModel(model);
+    assertEquals(model, cardScanner.getModel());
   }
 
   @Test
   void getAccessLogs() {
+    AccessLog accessLog = mock(AccessLog.class);
+    List<AccessLog> accessLogs = new ArrayList<>();
+    accessLogs.add(accessLog);
+    cardScanner.setAccessLogs(accessLogs);
   }
 
   @Test
   void getAccessControls() {
-  }
-
-  @Test
-  void setCardScannerId() {
-  }
-
-  @Test
-  void setSerialNo() {
-  }
-
-  @Test
-  void setMake() {
-  }
-
-  @Test
-  void setModel() {
-  }
-
-  @Test
-  void setAccessLogs() {
-  }
-
-  @Test
-  void setAccessControls() {
-  }
-
-  @Test
-  void testToString() {
+    AccessControl accessControl = mock(AccessControl.class);
+    List<AccessControl> accessControls = new ArrayList<>();
+    accessControls.add(accessControl);
+    cardScanner.setAccessControls(accessControls);
+    assertNotNull(cardScanner.getAccessControls());
+    assertEquals(1, cardScanner.getAccessControls().size());
+    assertSame(accessControl, cardScanner.getAccessControls().get(0));
   }
 }
