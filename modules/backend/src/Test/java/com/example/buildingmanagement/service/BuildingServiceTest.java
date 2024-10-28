b22d6dac9344bfdcf8f10a10aeeda2a4c18047ff
adesuwaE <adesuwa411@gmail.com>
2024-10-28 11:54:02 -0400

    "Build Service Class test"

--- modules/backend/src/Test/java/com/example/buildingmanagement/service/BuildingServiceTest.java
+++ modules/backend/src/Test/java/com/example/buildingmanagement/service/BuildingServiceTest.java
@@ -1,10 +1,39 @@
 package com.example.buildingmanagement.service;
 
+import com.example.buildingmanagement.dtos.BuildingRequestDTO;
+import com.example.buildingmanagement.entities.Building;
+import com.example.buildingmanagement.entities.Floor;
+import com.example.buildingmanagement.repository.BuildingRepository;
+import com.example.buildingmanagement.repository.FloorRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class BuildingServiceTest {
+
+  @Mock
+  private BuildingRepository buildingRepository;
+
+  @Mock
+  private FloorRepository floorRepository;
+
+  @InjectMocks
+  private BuildingService buildingService;
+
+  @BeforeEach
+  void setUp() {
+    MockitoAnnotations.openMocks(this);
+  }
 
   @Test
   void getAllBuildings() {
@@ -20,5 +49,23 @@ class BuildingServiceTest {
 
   @Test
   void deleteBuilding() {
+    //Since its just building ID, I don't have to do everything
+    BuildingRequestDTO buildingRequestDTO = new BuildingRequestDTO();
+    buildingRequestDTO.setBuildingId(10L);
+    buildingRequestDTO.setName("Avon hall");
+    buildingRequestDTO.setAddress("1217 bedford");
+    buildingRequestDTO.setFloorIds(List.of());
+
+
+   // doNothing().when(buildingRepository).deleteById(10L);
+
+    buildingService.deleteBuilding(10L);
+
+    //verify(buildingRepository).deleteById(10L);
+
+    //assertNotNull();
+    //assertEquals(10L, 10L);
+
+
   }
 }
