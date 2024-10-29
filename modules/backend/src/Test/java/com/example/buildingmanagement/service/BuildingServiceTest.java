2e048dc37a621ef8854566d91ec2ad63011c1016
adesuwaE <adesuwa411@gmail.com>
2024-10-29 16:33:30 -0400

    "updated BuildingService test"

--- modules/backend/src/Test/java/com/example/buildingmanagement/service/BuildingServiceTest.java
+++ modules/backend/src/Test/java/com/example/buildingmanagement/service/BuildingServiceTest.java
@@ -1,7 +1,7 @@
 package com.example.buildingmanagement.service;
 
-import com.example.buildingmanagement.dtos.BuildingDTO;
 import com.example.buildingmanagement.dtos.BuildingRequestDTO;
+import com.example.buildingmanagement.dtos.BuildingResponseDTO;
 import com.example.buildingmanagement.entities.Building;
 import com.example.buildingmanagement.entities.Floor;
 import com.example.buildingmanagement.repository.BuildingRepository;
@@ -15,6 +15,7 @@ import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -28,60 +29,101 @@ class BuildingServiceTest {
   @Mock
   private FloorRepository floorRepository;
 
-
   @InjectMocks
   private BuildingService buildingService;
 
   @BeforeEach
   void setUp() {
     MockitoAnnotations.openMocks(this);
+    buildingService = new BuildingService(buildingRepository, floorRepository);
   }
 
-  @Test
-  void getAllBuildings() {
-  }
 
   @Test
-  void getBuildingById() {
-    BuildingRequestDTO buildingRequestDTO = new BuildingRequestDTO();
-    buildingRequestDTO.setBuildingId(15L);
-
-   // when(buildingRepository.findById(15L)).thenReturn(new Building());
-
-    buildingService.getBuildingById(15L);
-    //verify(buildingRepository, times(1)).findById(15L);
-  }
-
-  @Test
-  void saveBuilding() {
-    Floor floor = new Floor();
-    floor.setFloorId(1L);
-
+  void createBuilding() {
+    Long buildingId = 15L;
+    String buildingName = "Avon Hall";
+    String buildingAddress = "1217 bedford";
+    BuildingResponseDTO buildingResponseDTO = new BuildingResponseDTO();
+    buildingResponseDTO.setBuildingId(buildingId);
+    buildingResponseDTO.setName(buildingName);
+    buildingResponseDTO.setAddress(buildingAddress);
 
     BuildingRequestDTO buildingRequestDTO = new BuildingRequestDTO();
-    buildingRequestDTO.setBuildingId(10L);
-    buildingRequestDTO.setName("Avon hall");
-    buildingRequestDTO.setAddress("1217 bedford");
+    buildingResponseDTO.setAddress(buildingAddress);
+    buildingRequestDTO.setName(buildingName);
+
+    Building building = new Building();
+    building.setBuildingId(buildingId);
+    building.setName(buildingName);
+    building.setAddress(buildingAddress);
+
+    //when(buildingRepository.save)
+
+    buildingService.createBuilding(buildingRequestDTO);
 
 
-//    Building building = new Building();
-//    building.setBuildingId(10L);
-//    building.setName("Avon hall");
-//    building.setAddress("1217 bedford");
-//    building.setFloors(List.of());
+    //verify(buildingRepository).save(any(Building.class));
+  }
 
-    BuildingDTO building = buildingService.saveBuilding(buildingRequestDTO);
 
-    //when(buildingRepository.save(any())).thenReturn(new BuildingRequestDTO[]);
+  @Test
+  void testGetAllBuildings() {
+    BuildingResponseDTO buildingResponseDTO = new BuildingResponseDTO();
+    buildingResponseDTO.setBuildingId(10L);
 
-    //verify(buildingRepository).save(any( Building.class));
+    // doNothing().when(buildingRepository).deleteById(10L);
+
+    buildingService.getAllBuildings();
+
   }
 
   @Test
-  void deleteBuilding() {
+  void testGetBuildingById() {
+    Long buildingId = 15L;
+    BuildingResponseDTO buildingResponseDTO = new BuildingResponseDTO();
+    buildingResponseDTO.setBuildingId(buildingId);
+
+    Building building = new Building();
+    building.setBuildingId(buildingId);
+
+     when(buildingRepository.findById(buildingId)).thenReturn(Optional.of(building));
+
+     buildingService.getBuildingById(buildingId);
+     verify(buildingRepository, times(1)).findById(buildingId);
+  }
+
+  @Test
+  void getLatestBuildingId() {
+    BuildingResponseDTO buildingResponseDTO = new BuildingResponseDTO();
+    buildingResponseDTO.setBuildingId(10L);
+
+
+    // doNothing().when(buildingRepository).deleteById(10L);
+
+    buildingService.getLatestBuildingId();
+  }
+
+  @Test
+  void updateBuilding() {
+    Long buildingId = 15L;
+    BuildingResponseDTO buildingResponseDTO = new BuildingResponseDTO();
+    buildingResponseDTO.setBuildingId(buildingId);
+
+    Building building = new Building();
+    building.setBuildingId(buildingId);
+
+    when(buildingRepository.findById(buildingId)).thenReturn(Optional.of(building));
+
+    buildingService.updateBuilding(buildingId, new BuildingRequestDTO());
+    verify(buildingRepository, times(1)).findById(buildingId);
+  }
+
+  @Test
+  void testDeleteBuilding() {
     //Since its just building ID, I don't have to do everything
-    BuildingRequestDTO buildingRequestDTO = new BuildingRequestDTO();
-    buildingRequestDTO.setBuildingId(10L);
+    BuildingResponseDTO buildingResponseDTO = new BuildingResponseDTO();
+    buildingResponseDTO.setBuildingId(10L);
 
     // doNothing().when(buildingRepository).deleteById(10L);
 
@@ -92,7 +134,6 @@ class BuildingServiceTest {
     // Works without assertEquals
     //assertNotNull();
     //assertEquals(10L, 10L);
-
-
   }
 }
+
