8d6fe179d977befec8ede8befae2d2016023af9b
StevenHernandez <steven.hernandez@peopleshores.com>
2024-10-30 19:49:09 -0400

    maintenance request is showing data

---
+++ modules/frontend/src/app/components/maintenancerequest/maintenancerequest.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MaintenancerequestComponent } from './maintenancerequest.component';
+
+describe('MaintenancerequestComponent', () => {
+  let component: MaintenancerequestComponent;
+  let fixture: ComponentFixture<MaintenancerequestComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MaintenancerequestComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(MaintenancerequestComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
