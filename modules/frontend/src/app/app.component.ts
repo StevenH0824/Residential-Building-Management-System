3e83886129abbd2af7e82ad842877750b16e737e
StevenHernandez <steven.hernandez@peopleshores.com>
2024-11-01 09:40:11 -0400

    removed responsive design and added header logo

--- modules/frontend/src/app/app.component.ts
+++ modules/frontend/src/app/app.component.ts
@@ -4,8 +4,7 @@ import { HeaderComponent } from "./layout/header/header.component";
 import { FooterComponent } from "./layout/footer/footer.component";
 import { CommonModule } from '@angular/common';
 import { ConfirmPopupModule } from 'primeng/confirmpopup';
-import { ElementRef, ViewChild, AfterViewInit } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
+
 
 
 @Component({
@@ -16,26 +15,11 @@ import { Router, NavigationEnd } from '@angular/router';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  isMenuOpen = false;
-  contentShift = "0";
-  constructor(private router: Router) {
-    this.router.events.subscribe(event => {
-      if (event instanceof NavigationEnd) {
-        this.adjustContentShift(event.url);
-      }
-    });
-  }
+  title = 'tutorial'
+  menuValue: boolean = false;
 
-  adjustContentShift(url: string) {
-    if (url === "/person") {
-      this.contentShift = "100px";
-    } else {
-      this.contentShift = "50px";
-    }
-  }
-
-  onMenuToggle(isOpen: boolean) {
-    this.isMenuOpen = isOpen;
+  toggleMenu(value:boolean){
+    this.menuValue = value;
   }
 }
 
